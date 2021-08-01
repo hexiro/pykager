18ea6ec08f1d03e0f95ff72628948f108be57ff5
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-08-01 18:39:34 -0400

    feat: make `readme_extensions` a dict + `content_type` property

--- pykager/lib/readme.py
+++ pykager/lib/readme.py
@@ -8,9 +8,16 @@ class Readme:
     readme_extensions = {
         # as according to https://github.com/github/markup/tree/master#markups
         # and https://packaging.python.org/guides/making-a-pypi-friendly-readme/#creating-a-readme-file
-        ".markdown", ".mdown", ".mkdn", ".md",  # Markdown
-        ".rst",  # reStructuredText
-        ".txt",  # plain text
+        # Markdown
+        ".markdown": "text/markdown",
+        ".mdown": "text/markdown",
+        ".mkdn": "text/markdown",
+        ".md": "text/markdown",
+        # Plain Text
+        ".txt": "text/plain",
+        "": "text/plain",
+        # reStructuredText
+        ".rst": "text/x-rst"
     }
 
     def __init__(self, directory: Path):
@@ -22,9 +29,20 @@ class Readme:
         Finds readme files in the specified directory
         README is case insensitive afaik
         """
-        search = [f for f in self.directory.glob("README.*") if f.suffix in self.readme_extensions]
-        return search[0] if search else None
+        for f in self.directory.glob("*"):
+            if f.name.upper().startswith("README") and f.suffix in self.readme_extensions:
+                return f
+
+    @cached_property
+    def content_type(self) -> Optional[str]:
+        if self.readme:
+            return self.readme_extensions[self.readme.suffix]
 
     @property
     def directory(self):
         return self.__directory
+
+
+if __name__ == "__main__":
+    r = Readme(Path.cwd().parents[1])
+    print(r.readme)
