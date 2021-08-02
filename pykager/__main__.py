745cee4ec70f4a2862df877f3a7ead8604ba6ef5
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-08-02 17:14:02 -0400

    refactor: use properties and args

--- pykager/__main__.py
+++ pykager/__main__.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 from pathlib import Path
+from pprint import pprint
 
 from pykager.lib import Git, Setup, Import
 from pykager.snippets import Snippet, Requirements, Readme
@@ -30,25 +31,89 @@ class Pykager:
         self.__setup_py = Setup(self.__input_dir)
 
         # setup.py things
-        self.name = self.__setup_py.name or self.__git.name
-        self.version = self.__setup_py.version
-        self.description = self.__setup_py.description
-        self.author = self.__setup_py.author or self.__git.author.name
-        self.author_email = self.__setup_py.author_email or self.__git.author.email
-        self.url = self.__setup_py.url or self.__git.url
-        self.license = self.__setup_py.license
-        self.long_description = Readme(self.__input_dir)
-        self.long_description_content_type = self.long_description.content_type
-        self.keywords = self.__setup_py.keywords
-        self.classifiers = self.__setup_py.classifiers
-        self.python_requires = self.__setup_py.python_requires
-        self.install_requires = Requirements(self.__input_dir)
-        self.zip_safe = self.__setup_py.zip_safe
-        self.packages = Packages(self)
+        self._name = None
+        self._version = None
+        self._description = None
+        self._author = None
+        self._author_email = None
+        self._url = None
+        self._license = None
+        self._long_description = None
+        self._long_description_content_type = None
+        self._keywords = None
+        self._classifiers = None
+        self._python_requires = None
+        self._install_requires = None
+        self._zip_safe = None
+        self._packages = None
+
+    @property
+    def name(self):
+        return self.__setup_py.name or self.__git.name
+
+    @property
+    def version(self):
+        return self.__setup_py.version
+
+    @property
+    def description(self):
+        return self.__setup_py.description
+
+    @property
+    def author(self):
+        return self.__setup_py.author or self.__git.author.name
+
+    @property
+    def author_email(self):
+        return self.__setup_py.author_email or self.__git.author.email
+
+    @property
+    def url(self):
+        return self.__setup_py.url or self.__git.url
+
+    @property
+    def license(self):
+        return self.__setup_py.license
+
+    @property
+    def long_description(self):
+        return Readme(self.__input_dir)
+
+    @property
+    def long_description_content_type(self):
+        return self.long_description.content_type
+
+    @property
+    def keywords(self):
+        return self.__setup_py.keywords
+
+    @property
+    def classifiers(self):
+        return self.__setup_py.classifiers
+
+    @property
+    def python_requires(self):
+        return self.__setup_py.python_requires
+
+    @property
+    def install_requires(self):
+        return Requirements(self.__input_dir)
+
+    @property
+    def zip_safe(self):
+        return self.__setup_py.zip_safe
+
+    @property
+    def packages(self):
+        return Packages(self)
 
     @property
     def setup_args(self) -> dict:
-        return {k: v for k, v in self.__dict__.items() if not k.startswith("_")}
+        args = ["name", "version", "description", "author", "author_email", "url", "license", "long_description",
+                "long_description_content_type", "keywords", "classifiers", "python_requires", "install_requires",
+                "zip_safe", "packages",
+                ]
+        return {k: getattr(self, f"_{k}") or getattr(self, k) for k in args}
 
     @property
     def code(self) -> str:
@@ -93,7 +158,7 @@ class Pykager:
             elif value.lower() in {"true", "false"}:
                 value = value.lower() == "true"
             if value:
-                setattr(self, arg, value)
+                setattr(self, "_" + arg, value)
 
         confirmation = input(
             "\n"
@@ -113,5 +178,4 @@ def main():
 
 
 if __name__ == "__main__":
-    p = Pykager()
-    print(p.code)
+    main()
