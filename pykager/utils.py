8756841212185facad200cb286ab7064eaeb4c18
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-08-04 16:05:46 -0400

    chore: remove unused func

--- pykager/utils.py
+++ pykager/utils.py
@@ -2,7 +2,6 @@ import ast
 import functools
 import json
 import platform
-import subprocess
 import sys
 from typing import Any
 
@@ -17,15 +16,6 @@ def cached_property(func):
     if sys.version_info > (3, 8):
         return functools.cached_property(func)
     return property(functools.lru_cache()(func))
-
-
-def clear():
-    """
-    Clears the console.
-    uses "cls" for windows
-    uses "clear" for unix
-    """
-    subprocess.Popen("cls" if is_windows else "clear", shell=True).wait()
 
 
 def safe_eval(data: str) -> Any:
